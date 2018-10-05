68c4bc42ff6cbcab20379660e2fec9bf757e43f9
Slava Torchik <torhik_slava@mail.ru>
2018-10-05 23:27:18 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,78 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.arr = [initialValue];    
   }
 
   add(number) {
-    // your implementation
+    this.arr.push('+', number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.arr.push('-', number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.arr.push('*', number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.arr.push('/', number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.arr.push('^', number);
+    return this;
   }
+
+  calculate() {
+    let result;
+    for (let i=this.arr.length-1; i>0; i--){
+      if(this.arr[i]==='^'){
+        result = Math.pow(this.arr[i-1],this.arr[i+1]);
+        this.arr.splice(i,2);
+        this.arr[i-1]=result;
+      }
+    }
+
+    for (let i=0; i<this.arr.length-1; i++){
+      if(this.arr[i]==='*'){
+        result = this.arr[i-1]*this.arr[i+1];
+        this.arr.splice(i,2);
+        this.arr[i-1]=result;
+      }
+    }
+
+    for (let i=0; i<this.arr.length-1; i++){
+      if(this.arr[i]==='/'){
+        result = this.arr[i-1]/this.arr[i+1];
+        this.arr.splice(i,2);
+        this.arr[i-1]=result;
+      }
+    }
+
+    for (let i=0; i<this.arr.length-1; i++){
+      if(this.arr[i]==='-'){
+        result = this.arr[i-1]-this.arr[i+1];
+        this.arr.splice(i,2);
+        this.arr[i-1]=result;
+      }
+    }
+
+    for (let i=0; i<this.arr.length-1; i++){
+      if(this.arr[i]==='+'){
+        result = this.arr[i-1]+this.arr[i+1];
+        this.arr.splice(i,2);
+        this.arr[i-1]=result;
+      }
+    }
+    result=this.arr[0]; 
+    return result;       
+  }
+  valueOf () {return this.calculate();}  
 }
 
 module.exports = SmartCalculator;
